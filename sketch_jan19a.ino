95a092adabfa8eac25399d2e211bb1fbe206e475
Giacomo <giacomo.marcon@barsanti.edu.it>
2019-01-23 13:30:28 +0100

    ProgettoSemaforo
    creo 2 semafori austriaci in un senso unico

---
+++ sketch_jan19a.ino
@@ -0,0 +1,75 @@
+int rossoA = 4;
+int gialloA = 3;
+int verdeA = 2;
+int rossoB = 5;
+int gialloB = 6;
+int verdeB = 7;
+
+void setup() {
+    pinMode(rossoA,OUTPUT);
+    pinMode(gialloA,OUTPUT);
+    pinMode(verdeA,OUTPUT);
+    pinMode(rossoB,OUTPUT);
+    pinMode(gialloB,OUTPUT);
+    pinMode(verdeB,OUTPUT);
+   }
+
+void lampeggiaVerdeB(){
+   digitalWrite (verdeB, HIGH);
+      delay  (1000);
+      digitalWrite (verdeB, LOW);
+      delay  (1000);
+  }
+
+void lampeggiaVerdeA(){
+ digitalWrite (verdeA, HIGH);
+      delay  (1000);
+      digitalWrite (verdeA, LOW);
+      delay  (1000);
+  
+}
+      
+void semaforo() {
+      digitalWrite (rossoA, HIGH);
+      digitalWrite (verdeB, HIGH);
+      digitalWrite (gialloA, LOW);
+      digitalWrite (gialloB, LOW);
+      digitalWrite (rossoB, LOW);
+      digitalWrite (verdeA, LOW);
+      delay  (10000);
+      
+      lampeggiaVerdeB();
+      lampeggiaVerdeB();
+      lampeggiaVerdeB();
+      lampeggiaVerdeB();
+      
+      
+      digitalWrite (rossoA, HIGH);
+      digitalWrite (gialloA, HIGH);
+      digitalWrite (gialloB, HIGH);
+      digitalWrite (verdeB, LOW);
+      delay  (2000);
+
+      digitalWrite (rossoB, HIGH);
+      digitalWrite (verdeA, HIGH);
+      digitalWrite (gialloA, LOW);
+      digitalWrite (gialloB, LOW);
+      digitalWrite (rossoA, LOW);
+      delay  (10000);
+
+      lampeggiaVerdeA();
+      lampeggiaVerdeA();
+      lampeggiaVerdeA();
+      lampeggiaVerdeA();
+      
+      digitalWrite (rossoB, HIGH);
+      digitalWrite (gialloB, HIGH);
+      digitalWrite (gialloA, HIGH);
+      digitalWrite (verdeA, LOW);
+      delay  (10000);
+
+      
+
+      
+    
+}    
